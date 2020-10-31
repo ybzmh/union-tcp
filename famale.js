156f0678316706d2298fd79779a4dc9f98b38412
ybzmh <ybzmh@qq.com>
2020-10-31 08:30:13 +0800

    ok2

--- famale.js
+++ famale.js
@@ -1,7 +1,8 @@
-let famale = require('./src/union-tcp-famale')
+let famale = require('./index').famale
+let publicip ='218.89.69.23'
 let config = {
     in: { host: "0.0.0.0", port: 60001 },
-    out: { host: "218.89.69.23", port: 60002 },
+    out: { host: publicip, port: 60002 },
     maps:[
         { port: 60003, remote: { id:'tjy',ip: "127.0.0.1", port: 8080} },
         { port: 60004, remote: { id:'tjy',ip: "10.0.133.218", port: 3389 }},
--- male.js
+++ male.js
@@ -1,5 +1,5 @@
-let male = require('./src/union-tcp-male')
-let conf = {host: "218.89.69.23", port: 60001,id:"tjy" }
+let male = require('./index').male
+let conf = {host: "218.89.69.23", port: 60001,id:"btoffice" }
 male.createNew(conf)
 
 
